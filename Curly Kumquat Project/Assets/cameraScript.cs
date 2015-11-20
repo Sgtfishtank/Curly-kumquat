2adf3ace2bd65c5dd462094650e9a887df2c2cfb
Nerfertius <nerfertius@gmail.com>
2015-11-20 22:35:31 +0100

    Basic Project

---
+++ Curly Kumquat Project/Assets/cameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(Keycode.A))
+        {
+
+        }
+    }
+}
