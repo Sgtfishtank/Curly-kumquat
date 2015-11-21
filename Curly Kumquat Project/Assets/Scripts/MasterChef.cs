6d0183b887c9fdd1f17c2e25e3e163edfd0e6e4d
Sgtfishtank <joh_lau@hotamil.com>
2015-11-21 01:10:39 +0100

    mastercheft cont

--- Curly Kumquat Project/Assets/Scripts/MasterChef.cs
+++ Curly Kumquat Project/Assets/Scripts/MasterChef.cs
@@ -1,11 +1,19 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class MasterChef : MonoBehaviour
 {
-	enum attacks {chop = 0, chopShove = 1, chopSwipe = 2, trippleChop = 3};
+	enum attacks {chop = 0, chopShove = 1, Swipe = 2, trippleChop = 3};
 	float mCooldown;
+	int currentAttack;
+	public float Limits;
+	float pos;
+	bool done;
+	public GameObject chopWarning;
+	public GameObject chopShoveWarning;
+	public GameObject swipeWarning;
+	public GameObject trippleChopWarning;
+	public GameObject knife;
 
 
 	// Use this for initialization
@@ -17,23 +25,60 @@ public class MasterChef : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		switch (Random.Range(0,4))
+		if(currentAttack == -1 && Time.time > mCooldown)
 		{
-		case 0:
-			Chop();
-			break;
-		case 1:
-			ChopShove();
-			break;
-		case 2:
-			ChopSwipe();
-			break;
-		case 3:
-			TrippleChop();
-			break;
-		default:
-			break;
-		}		
+			switch (UnityEngine.Random.Range(0,4))
+			{
+			case 0:
+				currentAttack = 0;
+				pos = Random.Range(-Limits,Limits+0.1f);
+				Destroy( Instantiate(chopWarning,new Vector3(pos,0,0),Quaternion.identity),1.1f);
+				Invoke("Chop" ,1);
+				break;
+			case 1:
+				ChopShove();
+				break;
+			case 2:
+				Swipe();
+				break;
+			case 3:
+				TrippleChop();
+				break;
+			default:
+				break;
+			}
+		}
+		if(currentAttack != -1)
+		{
+			switch (UnityEngine.Random.Range(0,4))
+			{
+			case 0:
+				Chop();
+				break;
+			case 1:
+				ChopShove();
+				break;
+			case 2:
+				Swipe();
+				break;
+			case 3:
+				TrippleChop();
+				break;
+			default:
+				break;
+			}
+		}
+		if(done)
+		{
+			if(knife.transform.rotation.eulerAngles.z != -90)
+				knife.transform.RotateAround(new Vector3(0,0,-5), new Vector3(0,0,1), -30 * Time.deltaTime);
+			else
+			{
+				mCooldown = Random.Range(3,10) + Time.time;
+				done = false;
+				currentAttack = -1;
+			}
+		}
 	}
 
 	public void Reset ()
@@ -43,13 +88,18 @@ public class MasterChef : MonoBehaviour
 
 	void Chop()
 	{
+		knife.transform.position = new Vector3(pos, transform.position.y, transform.position.z);
+		if(knife.transform.rotation.eulerAngles.z != 0)
+			knife.transform.RotateAround(new Vector3(0,0,-5), new Vector3(0,0,1), 30 * Time.deltaTime);
+		else
+			done = true;
 
 	}
 	void ChopShove()
 	{
 
 	}
-	void ChopSwipe()
+	void Swipe()
 	{
 
 	}
