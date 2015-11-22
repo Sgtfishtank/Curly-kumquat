c137b8f1d69deb73401b398dda067a23c8c7ce76
Thism2 <thism.rasse@hotmail.com>
2015-11-22 21:31:09 +0100

    Buttons.

---
+++ Curly Kumquat Project/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour 
+{
+	public GameObject buttonsource;
+	public MeshRenderer mr;
+	public Button but;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		mr = buttonsource.GetComponent<MeshRenderer> ();
+		but = GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		mr.material.color = but.colors.pressedColor;
+	}
+}
