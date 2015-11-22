95c1980fadf892b163fa1e0c6ab0a84c72f6242e
Thism2 <thism.rasse@hotmail.com>
2015-11-22 18:45:10 +0100

    Fix.

--- Curly Kumquat Project/Assets/Scripts/cameraScript.cs
+++ Curly Kumquat Project/Assets/Scripts/cameraScript.cs
@@ -3,14 +3,26 @@ using System.Collections;
 
 public class cameraScript : MonoBehaviour
 {
-	private Vector3 pos;
-	private Quaternion rot;
+	public GameObject MenuCamera;
+
+	private Vector3 menipos;
+	private Quaternion menirot;
+
+	private Vector3 gamepos;
+	private Quaternion gamerot;
 	//private Camera mCamera;
 
 	void Awake()
 	{
-		pos = transform.position;
-		rot = transform.rotation;
+		menipos = MenuCamera.transform.position;
+		menirot = MenuCamera.transform.rotation; 
+
+		// REOMVE KEBAB
+		menipos = transform.position;
+		menirot = transform.rotation;
+
+		gamepos = transform.position;
+		gamerot = transform.rotation;
 		//mCamera = GetComponent<Camera>();
 	}
 
@@ -21,14 +33,17 @@ public class cameraScript : MonoBehaviour
 
     void Update()
     {
-		transform.position = pos;
+		transform.position = gamepos;
 		if (Game.Instance.CurrentState() == Game.State.Menu) 
 		{
-			transform.rotation = Quaternion.Euler(rot.eulerAngles.x, rot.eulerAngles.y + (Mathf.Sin(Time.time) * 15), rot.eulerAngles.z);
+			transform.position = menipos;
+			transform.rotation = menirot; 
+			//transform.rotation = Quaternion.Euler(gamerot.eulerAngles.x, gamerot.eulerAngles.y + (Mathf.Sin(Time.time) * 15), gamerot.eulerAngles.z);
 		}
 		else 
 		{
-			transform.rotation = Quaternion.Euler(rot.eulerAngles.x, rot.eulerAngles.y, rot.eulerAngles.z);
+			transform.position = Vector3.MoveTowards(transform.position, gamepos, Time.deltaTime * 10);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, gamerot, Time.deltaTime * 10);
 		}
 	}
 }
